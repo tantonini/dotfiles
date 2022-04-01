e8c25a3e312f1a3bb0d7023366bf1492cb83bd79
Thomas Antonini <antonini.thomas@gmail.com>
2022-04-01 23:07:55 +0200

    nvim: Add gitsigns
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -60,6 +60,9 @@ return require('packer').startup(function(use)
     run = ":TSUpdate"
   }
 
+  -- Git
+  use "lewis6991/gitsigns.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
