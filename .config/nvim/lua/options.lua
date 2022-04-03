52f3e0208c500c18126af89ece104ca5a08b8491
Thomas Antonini <antonini.thomas@gmail.com>
2022-04-03 13:45:05 +0200

    nvim: Tabline show cwd
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -1,3 +1,37 @@
+vim.api.nvim_command([[
+    function MyTabLabel(n)
+      return substitute(getcwd(-1, a:n),$HOME,'~','')
+	endfunction
+
+	function MyTabLine()
+	  let s = ''
+	  for i in range(tabpagenr('$'))
+	    " select the highlighting
+	    if i + 1 == tabpagenr()
+	      let s ..= '%#TabLineSel#'
+	    else
+	      let s ..= '%#TabLine#'
+	    endif
+
+	    " set the tab page number (for mouse clicks)
+	    let s ..= '%' .. (i + 1) .. 'T'
+
+	    " the label is made by MyTabLabel()
+	    let s ..= ' %{MyTabLabel(' .. (i + 1) .. ')} '
+	  endfor
+
+	  " after the last tab fill with TabLineFill and reset tab page nr
+	  let s ..= '%#TabLineFill#%T'
+
+	  " right-align the label to close the current tab page
+	  if tabpagenr('$') > 1
+	    let s ..= '%=%#TabLine#%999XX'
+	  endif
+
+	  return s
+	endfunction
+]])
+
 local options = {
   backup = false,                          -- creates a backup file
   cmdheight = 2,                           -- more space in the neovim command line for displaying messages
@@ -35,6 +69,7 @@ local options = {
   updatetime = 300,                        -- faster completion (4000ms default)
   -- wrap = false,                            -- display lines as one long line
   -- writebackup = false,                     -- if a file is being edited by another program (or was written to file while editing with another program), it is not allowed to be edited
+  tabline = "%!MyTabLine()"
 }
 
 for k, v in pairs(options) do
