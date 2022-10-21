1a5768b441bfc094298a92f109296df530d8556b
Thomas Antonini <antonini.thomas@gmail.com>
2022-10-21 14:49:50 +0200

    nvim: Simplify treesitter
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -5,11 +5,13 @@ end
 
 configs.setup {
   ensure_installed = "all",
-  sync_install = false,
-  ignore_install = { "" }, -- List of parsers to ignore installing
   highlight = {
     enable = true, -- false will disable the whole extension
-    disable = { "" }, -- list of language that will be disabled
-    additional_vim_regex_highlighting = true,
-  }
+  },
+	autopairs = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+	},
 }
