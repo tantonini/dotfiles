acd17a0072faf263ebaa0ea9a13916c2e69b9dfc
Thomas Antonini <antonini.thomas@gmail.com>
2022-10-30 19:39:16 +0100

    nvim: Add virt-column
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -85,6 +85,9 @@ return require('packer').startup(function(use)
   -- Config per project
   use 'windwp/nvim-projectconfig'
 
+  -- Character for colorcolumn
+  use "lukas-reineke/virt-column.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
