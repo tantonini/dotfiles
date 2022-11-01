4182b1f0f264385199195afb2655474f8c07845f
Thomas Antonini <antonini.thomas@gmail.com>
2022-11-01 10:30:12 +0100

    nvim: Add projectconfig autocmd group
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins/nvim-projectconfig.lua
+++ .config/nvim/lua/plugins/nvim-projectconfig.lua
@@ -3,4 +3,16 @@ if not status_ok then
   return
 end
 
+local projectconfig_grp = vim.api.nvim_create_augroup("projectconfig", { clear = true })
+
 project_config.setup()
+
+-- Clear and reload project config on each tab change to avoid config
+-- applied to another project
+local clear_projectconfig_cb = function()
+  vim.api.nvim_clear_autocmds({ group = projectconfig_grp })
+end
+
+vim.api.nvim_create_autocmd("TabLeave", {
+  callback = clear_projectconfig_cb,
+})
