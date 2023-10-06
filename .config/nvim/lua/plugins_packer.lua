501a1e7ef4a5f9e39d2b99681dea2142ae709f17
Thomas Antonini <antonini.thomas@gmail.com>
2023-10-06 14:59:54 +0200

    nvim: lazy - Add nvim-cmp
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins_packer.lua
+++ .config/nvim/lua/plugins_packer.lua
@@ -26,19 +26,6 @@ return require('packer').startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
 
-  -- cmp plugins
-  use "hrsh7th/nvim-cmp" -- The completion plugin
-  use "hrsh7th/cmp-buffer" -- buffer completions
-  use "hrsh7th/cmp-path" -- path completions
-  use "hrsh7th/cmp-cmdline" -- cmdline completions
-  use "saadparwaiz1/cmp_luasnip" -- snippet completions
-  use "hrsh7th/cmp-nvim-lsp"
-  use "hrsh7th/cmp-nvim-lua"
-
-  -- snippets
-  use "L3MON4D3/LuaSnip" --snippet engine
-  use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
-
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
 
