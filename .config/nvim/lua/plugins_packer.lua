07faf3ea42108f010d13eee4ff4eb61c50626af3
Thomas Antonini <antonini.thomas@gmail.com>
2023-11-08 19:52:16 +0100

    nvim: Remove nvim-projectconfig
    Embedded in nvim 0.9
    https://neovim.io/doc/user/editorconfig.html
    
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins_packer.lua
+++ .config/nvim/lua/plugins_packer.lua
@@ -29,9 +29,6 @@ return require('packer').startup(function(use)
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
 
-  -- Config per project
-  use 'windwp/nvim-projectconfig'
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
