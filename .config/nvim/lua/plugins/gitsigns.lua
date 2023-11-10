ddbcf6767feea945ebc013d8223e0c67b182ab10
Thomas Antonini <antonini.thomas@gmail.com>
2023-11-10 08:34:26 +0100

    nvim: Rework gitsigns
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -1,14 +1,20 @@
-return {
+local M = {
   "lewis6991/gitsigns.nvim",
   event = { "BufReadPost", "BufNewFile", "BufWritePre" },
-  opts = {
-    signs = {
-      add          = { text = '+' },
-      change       = { text = '~' },
-      delete       = { text = '-' },
-      topdelete    = { text = '-' },
-      changedelete = { text = '~' },
-      untracked    = { text = '┆' },
-    }
-  }
 }
+
+M.config = function()
+  require('gitsigns').setup()
+end
+
+return M
+
+-- My config
+-- signs = {
+--   add          = { text = '+' },
+--   change       = { text = '~' },
+--   delete       = { text = '-' },
+--   topdelete    = { text = '-' },
+--   changedelete = { text = '~' },
+--   untracked    = { text = '┆' },
+-- }
