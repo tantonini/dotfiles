0c15958aa2b7e62128b865b2133e3a10ee24202a
Thomas Antonini <antonini.thomas@gmail.com>
2023-11-11 11:02:36 +0100

    nvim: Remove terminal keymaps (not used)
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/core/keymaps.lua
+++ .config/nvim/lua/core/keymaps.lua
@@ -1,7 +1,5 @@
 local opts = { noremap = true, silent = true }
 
-local term_opts = { silent = true }
-
 -- Shorten function name
 local keymap = vim.api.nvim_set_keymap
 
@@ -9,9 +7,3 @@ local keymap = vim.api.nvim_set_keymap
 keymap("", "<Space>", "<Nop>", opts)
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-
--- Toggleterm
--- Use a different terminal per tab
-keymap("n", "<c-t>", "<cmd>exe tabpagenr() . 'ToggleTerm'<cr>", opts)
-keymap("i", "<c-t>", "<cmd>exe tabpagenr() . 'ToggleTerm'<cr>", opts)
-keymap("t", "<c-t>", "<cmd>exe tabpagenr() . 'ToggleTerm'<cr>", opts)
