65adebb0cee4624acb91f51a6c5a9d1019072dff
Thomas Antonini <antonini.thomas@gmail.com>
2023-11-29 10:01:08 +0100

    nvim: nvim-cmp remove completino for : and /
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins/nvim-cmp.lua
+++ .config/nvim/lua/plugins/nvim-cmp.lua
@@ -88,22 +88,6 @@ M.config = function()
         return vim_item
       end,
     },
-    -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-    cmp.setup.cmdline({ '/', '?' }, {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = 'buffer' }
-      }
-    }),
-    -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-    --cmp.setup.cmdline(':', {
-    --  mapping = cmp.mapping.preset.cmdline(),
-    --  sources = cmp.config.sources({
-    --    { name = 'path' }
-    --  }, {
-    --    { name = 'cmdline' }
-    --  })
-    --})
   })
 end
 
