16f2011f0ac3fc37c72d4f9547fb27d78a9dfb3a
Thomas Antonini <antonini.thomas@gmail.com>
2025-10-24 11:19:28 +0200

    nvim: Update lsp config to new way
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins/nvim-lspconfig.lua
+++ .config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -5,52 +5,12 @@ local M = {
 }
 
 M.config = function()
-  local lspconfig = require('lspconfig')
   local has_cmp, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
-  local capabilities = vim.tbl_deep_extend(
-    "force",
-    {},
-    vim.lsp.protocol.make_client_capabilities(),
-    has_cmp and cmp_nvim_lsp.default_capabilities() or {}
-  )
 
   local servers = { 'bashls', 'clangd', 'cmake', 'lua_ls', 'pyright', 'rust_analyzer' }
   for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup {
-      capabilities = capabilities,
-    }
+    vim.lsp.enable(lsp)
   end
-
-  lspconfig.lua_ls.setup {
-    on_init = function(client)
-      local path = client.workspace_folders[1].name
-      if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
-        client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
-          Lua = {
-            runtime = {
-              -- Tell the language server which version of Lua you're using
-              -- (most likely LuaJIT in the case of Neovim)
-              version = 'LuaJIT'
-            },
-            -- Make the server aware of Neovim runtime files
-            workspace = {
-              checkThirdParty = false,
-              library = {
-                vim.env.VIMRUNTIME
-                -- "${3rd}/luv/library"
-                -- "${3rd}/busted/library",
-              }
-              -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-              -- library = vim.api.nvim_get_runtime_file("", true)
-            }
-          }
-        })
-
-        client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-      end
-      return true
-    end
-  }
 
   -- Use LspAttach autocommand to only map the following keys
   -- after the language server attaches to the current buffer
