b04ec07b4735957c9735aca20822d70700878763
Thomas Antonini <antonini.thomas@gmail.com>
2025-11-14 15:34:53 +0100

    nvim: Update treesitter
    Signed-off-by: Thomas Antonini <antonini.thomas@gmail.com>

--- .config/nvim/lua/plugins/nvim-treesitter.lua
+++ .config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,26 +1,8 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
+  lazy = false,
+  branch = 'main',
   build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile", "BufWritePre", "VeryLazy" },
-  cmd = { "TSUpdateSync", "TSUpdate", "TSInstall" },
 }
-
-M.config = function()
-    require'nvim-treesitter.configs'.setup {
-      ensure_installed = "all",
-      sync_install = false,
-      highlight = {
-        enable = true,
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
-      },
-      indent = { enable = false },
-    }
-end
 
 return M
